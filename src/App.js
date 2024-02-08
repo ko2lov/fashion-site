fd0b119fad2a89e263d43bb39c2d50f5553c8d2f
KOZZLIN4 <ko2lov.r@yandex.ru>
2024-02-08 21:51:23 +0300

    Почистьил

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+
+  const project = 'Fasion'
+
   return (
     <div className="App">
         <h1>Hello React!!</h1>
+        <h2>{project}</h2>
     </div>
   );
 }
