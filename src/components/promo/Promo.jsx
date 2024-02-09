594ff09e3b18f0ee2e22d3428930bd8385a0e655
KOZZLIN4 <ko2lov.r@yandex.ru>
2024-02-09 12:58:18 +0300

    highlight

--- src/components/promo/Promo.jsx
+++ src/components/promo/Promo.jsx
@@ -11,8 +11,14 @@ const Promo = () => {
             <div className="promo__content">
                 <div className="promo__text">
                     <div className="promo__title">
-                        <span className='highlight'>LET’S</span>
-                         EXPLORE UNIQUE CLOTHES.
+                        <span className='highlight'>
+                            <span>LET’S</span>
+                        </span>
+                         EXPLORE 
+                         <span className='highlight__yellow'>
+                            <span>UNIQUE</span>
+                        </span> 
+                         CLOTHES.
                     </div>
                     <div className='promo__desc'>Live for Influential and Innovative fashion!</div>
                     <div className='prom__btn-wrapper'>
